c81501b1d67a3bb8446ee8efcb36c0cdb39ae9f2
namrapithwa-tech <164359724+namrapithwa-tech@users.noreply.github.com>
2025-01-11 08:21:17 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace jQuery_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
